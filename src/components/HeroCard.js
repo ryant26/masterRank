b1347181567a9d9e502e4b5fbfbbc68088fb14be
Alex Swizzl <alex.swizzl@gmail.com>
2017-06-12 18:34:19 -0700

    Add plus button when hero card is hovered. Also updated style

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,6 +1,7 @@
 import React, {
   Component
 } from 'react';
+import FontAwesome from 'react-fontawesome'
 
 import HeroCardStats from './HeroCardStats.js'
 
@@ -10,7 +11,7 @@ export default class HeroCard extends Component {
     super(props);
     this.state = {
       isStatsToggleOn: false,
-      hovering: false,
+      isHovering: false,
     };
 
     this.toggleStats = this.toggleStats.bind(this);
@@ -25,41 +26,28 @@ export default class HeroCard extends Component {
 
   toggleHover() {
     this.setState(prevState => ({
-      hovering: !prevState.hovering
+      isHovering: !prevState.isHovering
     }))
   }
 
   render() {
     const containerStyle = {
-      'backgroundColor': '#fff',
+      'display': 'flex',
+      'backgroundColor': '#66ccff',
       'width': '720px',
-      'padding': '5px',
-      // 'border': '5px solid red',
+      'margin': '10px',
     }
 
     const heroIconStyle = {
-      'float': 'left',
-      'textAlign': 'center',
+      'backgroundColor': 'lightgrey',
+      'color': 'darkgrey',
       'height': '60px',
-      'width': '25%',
+      'width': '60px',
+      'float': 'left',
+      'fontSize': '48px',
     }
 
-    const SRStyle = {
-      'float': 'left',
-      'textAlign': 'center',
-      'height': '60px',
-      'width': '25%',
-    }
-
-    const gamesPlayedStyle = {
-      'float': 'left',
-      'textAlign': 'center',
-      'height': '60px',
-      'width': '25%',
-    }
-
-    const winPercentageStyle = {
-      'float': 'left',
+    const boxStyle = {
       'textAlign': 'center',
       'height': '60px',
       'width': '25%',
@@ -68,27 +56,40 @@ export default class HeroCard extends Component {
     let winPercentage = parseFloat(this.props.hero.general_stats.win_percentage * 100.0).toFixed(1);
 
     return (
-      <div style={containerStyle}
-        onClick={this.toggleStats}
-        onMouseEnter={this.toggleHover}
-        onMouseLeave={this.toggleHover}>
-        <div style={heroIconStyle}>{this.props.hero.name}</div>
+      <div>
+        <div style={containerStyle}
+          onClick={this.toggleStats}
+          onMouseEnter={this.toggleHover}
+          onMouseLeave={this.toggleHover}>
 
-        <div style={SRStyle}>
-          <div>Rank</div>
-          <div>2734</div>
+          <div style={boxStyle}>
+            {
+              this.state.isHovering
+              ? <button style={heroIconStyle}>
+                  <FontAwesome name='plus' />
+                </button>
+              : <img style={heroIconStyle}
+                  src={require(`../assets/${this.props.hero.name}-icon.png`)}
+                  alt = {this.props.hero.name+' icon'}
+                />
+            }
+          </div>
+
+          <div style={boxStyle}>
+            <div>Rank</div>
+            <div>2734</div>
+          </div>
+
+          <div style={boxStyle}>
+            <div>Games Played</div>
+            <div>{this.props.hero.general_stats.games_played}</div>
+          </div>
+
+          <div style={boxStyle}>
+            <div>WIN %</div>
+            <div>{winPercentage}%</div>
+          </div>
         </div>
-
-        <div style={gamesPlayedStyle}>
-          <div>Games Played</div>
-          <div>{this.props.hero.general_stats.games_played}</div>
-        </div>
-
-        <div style={winPercentageStyle}>
-          <div>WIN %</div>
-          <div>{winPercentage}%</div>
-        </div>
-
         {
           this.state.isStatsToggleOn
           ? <HeroCardStats hero={this.props.hero}/>
