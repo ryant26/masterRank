d73019c7932c836aa6222f6a53ad5fb79e652f07
Alex Swizzl <alex.swizzl@gmail.com>
2017-06-13 20:28:00 -0700

    Add Redux store to MasterRank.js

---
+++ src/containers/MasterRank.js
@@ -0,0 +1,49 @@
+import React, {
+  Component
+} from 'react';
+import Helmet from 'react-helmet';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as HeroActions from '../actions/hero.js';
+import Header from '../components/Header.js';
+import HeroCard from '../components/HeroCard.js';
+
+class MasterRank extends Component {
+  render() {
+    const { dispatch, heroes } = this.props;
+    const addHero = bindActionCreators(HeroActions.addHero, dispatch)
+
+    const heroCardComponents = heroes.map(function(hero) {
+      return <HeroCard hero={hero} key={hero.name} />
+    })
+
+    return (
+        <div className="App">
+          <Helmet
+            link={[
+              {
+                rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+              }
+            ]}
+          />
+
+          <div className="App-header">
+            <h2>Master Rank</h2>
+          </div>
+
+          <div className="HeroCard">
+              { heroCardComponents }
+          </div>
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  {
+    heroes: state.heroes
+  }
+);
+
+export default connect(mapStateToProps)(MasterRank);
