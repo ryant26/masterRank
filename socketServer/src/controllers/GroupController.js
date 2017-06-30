c7a9c651b6bbf8aa2ab43ecf60044522a49ac288
Ryan Thornhill <ryan.thornhill@ge.com>
2017-06-30 09:22:51 -0600

    Add Group Controller

---
+++ socketServer/src/controllers/GroupController.js
@@ -0,0 +1,12 @@
+/**
+ * This object handles group related API requests
+ * @param config.region - Region
+ * @param config.socket - Socket
+ * @param config.RedisClient - Redis Client
+ * @constructor
+ */
+const GroupController = function (config) {
+
+};
+
+module.exports = GroupController;
