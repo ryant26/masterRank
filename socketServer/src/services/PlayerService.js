689daf3c71df67c1b7e47d9d929789bbfc458812
Ryan Thornhill <ryan.thornhill@ge.com>
2017-08-02 19:04:57 +0530

    Converted Group Controller to Service

--- socketServer/src/services/PlayerService.js
+++ socketServer/src/services/PlayerService.js
@@ -3,6 +3,13 @@ let clientEvents = require('../socketEvents/clientEvents');
 let PlayerClient = require('../apiClients/PlayerClient');
 let RedisClient = require('../apiClients/RedisClient');
 
+/**
+ * Fires the initialData event on the client and provides the hero Meta lists
+ * @param battleNetId
+ * @param rank
+ * @param region
+ * @param socket
+ */
 let sendInitialData = function (battleNetId, rank, region, socket) {
     PlayerClient.getPlayerRank(battleNetId, region).then((rankObj) => {
         let rank = rankObj.rank;
@@ -15,10 +22,14 @@ let sendInitialData = function (battleNetId, rank, region, socket) {
     });
 };
 
-let getPlayerRank = function (battleNetId, region) {
-    return PlayerClient.getPlayerRank(battleNetId, region);
-};
-
+/**
+ * Adds a hero to a player and the Meta lists by name (looks up stats)
+ * @param battleNetId
+ * @param rank
+ * @param region
+ * @param namespace
+ * @param heroName
+ */
 let addHeroByName = function(battleNetId, rank, region, namespace, heroName) {
     PlayerClient.getHeroStats(battleNetId, region.name, heroName).then((stats) => {
         let heroObj = {heroName: heroName, stats, battleNetId: battleNetId};
@@ -29,12 +40,27 @@ let addHeroByName = function(battleNetId, rank, region, namespace, heroName) {
     });
 };
 
+/**
+ * Removes all heros that a player has added from meta lists and player
+ * @param battleNetId
+ * @param rank
+ * @param region
+ * @param namespace
+ */
 let removeAllPlayerHeros = function(battleNetId, rank, region, namespace) {
     RedisClient.getPlayerHeros(battleNetId).then((heros) => {
         return removePlayerHeros(battleNetId, rank, region, namespace, ...heros);
     });
 };
 
+/**
+ * Removes the specified heros from the player and the meta lists
+ * @param battleNetId
+ * @param rank
+ * @param region
+ * @param namespace
+ * @param heros
+ */
 let removePlayerHeros = function(battleNetId, rank, region, namespace, ...heros) {
     RedisClient.removePlayerHeros(battleNetId, ...heros);
     RedisClient.removeMetaHeros(rank, region, ...heros).then(() => {
@@ -43,6 +69,10 @@ let removePlayerHeros = function(battleNetId, rank, region, namespace, ...heros)
         });
     });
     logger.info(`Player [${battleNetId}] left rank [${rank}]`);
+};
+
+let getPlayerRank = function (battleNetId, region) {
+    return PlayerClient.getPlayerRank(battleNetId, region);
 };
 
 module.exports = {
