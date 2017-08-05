62f13a24b8f498e78afedb628319166365018bd2
Ryan Thornhill <ryan.thornhill@ge.com>
2017-08-05 15:51:35 +0530

    Add BaseController + tests

---
+++ socketServer/src/controllers/BaseController.js
@@ -0,0 +1,83 @@
+const logger = require('winston');
+const EventEmitter = require('events');
+
+module.exports = class BaseController {
+
+    constructor(config) {
+        this.config = config;
+        this.eventEmitter = new EventEmitter();
+        this.socket = config.socket;
+        this.token = config.token;
+        this.battleNetId = this.token.battleNetId;
+        this.region = config.region;
+        this.namespace = config.namespace;
+        this.groupId;
+    }
+
+    getBeforeEvent(event) {
+        return `before.${event}`;
+    }
+
+    getOnEvent(event) {
+        return `on.${event}`;
+    }
+
+    getAfterEvent(event) {
+        return `after.${event}`;
+    }
+
+    addSocketHandler(event) {
+        if (!this.socket._callbacks[event]) {
+            this.socket.on(event, (eventData) => {
+                let data = {
+                    'config': this.config,
+                    eventData
+                };
+
+                try {
+                    this.eventEmitter.emit(this.getBeforeEvent(event), data);
+                    this.eventEmitter.emit(this.getOnEvent(event), data);
+                    this.eventEmitter.emit(this.getAfterEvent(event), data);
+                } catch (err) {
+                    logger.error(`There was an error handling event ${event}: ${err}`);
+                }
+            });
+        }
+    }
+
+    before(events, ...handlers) {
+        this._addEventHandler(events, this.getBeforeEvent, ...handlers);
+    }
+
+    on(events, ...handlers) {
+        this._addEventHandler(events, this.getOnEvent, ...handlers);
+    }
+
+    after(events, ...handlers) {
+        this._addEventHandler(events, this.getAfterEvent, ...handlers);
+    }
+
+    _addEventHandler(socketEvents, controllerEventConverter, ...handlers) {
+        if (Array.isArray(socketEvents)) {
+            socketEvents.forEach((event) => {
+                this.addSocketHandler(event);
+            });
+
+            let controllerEvents = socketEvents.map((event) => {
+                return controllerEventConverter(event);
+            });
+
+            controllerEvents.forEach((event) => {
+                handlers.forEach((handler) => {
+                    this.eventEmitter.on(event, handler);
+                });
+            });
+
+        } else {
+            this.addSocketHandler(socketEvents);
+            handlers.forEach((handler) => {
+                this.eventEmitter.on(controllerEventConverter(socketEvents), handler);
+            });
+        }
+    }
+};
