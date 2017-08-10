4e9a5f2f9ff01ccc9608c6cd6ddc1285b7188fee
Ryan Thornhill <ryan.thornhill@ge.com>
2017-08-10 17:42:55 +0530

    Add comments to base controller

--- socketServer/src/controllers/BaseController.js
+++ socketServer/src/controllers/BaseController.js
@@ -14,19 +14,69 @@ module.exports = class BaseController {
         this.namespace = config.namespace;
     }
 
-    getBeforeEvent(event) {
+    /**
+     * Adds "before" handler(s) to the passed events
+     * @param {String || []} events - Array of events or single event string
+     * @param handlers - handler functions
+     */
+    before(events, ...handlers) {
+        this._addEventHandler(events, this._getBeforeEvent, ...handlers);
+    }
+
+    /**
+     * Adds "on" handler(s) to the passed events
+     * @param {String || []} events - Array of events or single event string
+     * @param handlers - handler functions
+     */
+    on(events, ...handlers) {
+        this._addEventHandler(events, this._getOnEvent, ...handlers);
+    }
+
+    /**
+     * Adds "after" handler(s) to the passed events
+     * @param {String || []} events - Array of events or single event string
+     * @param handlers - handler functions
+     */
+    after(events, ...handlers) {
+        this._addEventHandler(events, this._getAfterEvent, ...handlers);
+    }
+
+    /**
+     * Convert passed event name to "before" event
+     * @param event
+     * @returns {string}
+     * @private
+     */
+    _getBeforeEvent(event) {
         return `before.${event}`;
     }
 
-    getOnEvent(event) {
+    /**
+     * Convert passed event name to "on" event
+     * @param event
+     * @returns {string}
+     * @private
+     */
+    _getOnEvent(event) {
         return `on.${event}`;
     }
 
-    getAfterEvent(event) {
+    /**
+     * Convert passed event to "after" event
+     * @param event
+     * @returns {string}
+     * @private
+     */
+    _getAfterEvent(event) {
         return `after.${event}`;
     }
 
-    addSocketHandler(event) {
+    /**
+     * Adds a handler to the controller socket for the passed event.
+     * @param event
+     * @private
+     */
+    _addSocketHandler(event) {
         if (!this.socket._events || !this.socket._events[event]) {
             this.socket.on(event, (eventData) => {
                 let data = {
@@ -34,10 +84,10 @@ module.exports = class BaseController {
                     eventData
                 };
 
-                this.eventEmitter.emitAsync(this.getBeforeEvent(event), data).then(() => {
-                    return this.eventEmitter.emit(this.getOnEvent(event), data);
+                this.eventEmitter.emitAsync(this._getBeforeEvent(event), data).then(() => {
+                    return this.eventEmitter.emit(this._getOnEvent(event), data);
                 }).then(() => {
-                    return this.eventEmitter.emit(this.getAfterEvent(event), data);
+                    return this.eventEmitter.emit(this._getAfterEvent(event), data);
                 }).catch((error) => {
                     logger.error(`Error handling socket event [${event}] for user [${this.battleNetId}]: ${error}`);
                     if (clientEvents.error[event]) {
@@ -50,22 +100,17 @@ module.exports = class BaseController {
         }
     }
 
-    before(events, ...handlers) {
-        this._addEventHandler(events, this.getBeforeEvent, ...handlers);
-    }
-
-    on(events, ...handlers) {
-        this._addEventHandler(events, this.getOnEvent, ...handlers);
-    }
-
-    after(events, ...handlers) {
-        this._addEventHandler(events, this.getAfterEvent, ...handlers);
-    }
-
+    /**
+     * Private function to map eventemitter handlers to socket events
+     * @param socketEvents
+     * @param controllerEventConverter
+     * @param handlers
+     * @private
+     */
     _addEventHandler(socketEvents, controllerEventConverter, ...handlers) {
         if (Array.isArray(socketEvents)) {
             socketEvents.forEach((event) => {
-                this.addSocketHandler(event);
+                this._addSocketHandler(event);
             });
 
             let controllerEvents = socketEvents.map((event) => {
@@ -79,7 +124,7 @@ module.exports = class BaseController {
             });
 
         } else {
-            this.addSocketHandler(socketEvents);
+            this._addSocketHandler(socketEvents);
             handlers.forEach((handler) => {
                 this.eventEmitter.on(controllerEventConverter(socketEvents), handler);
             });
