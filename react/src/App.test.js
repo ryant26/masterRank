d4638c5607fccc8f272e118d90d6d2f30459c10a
Scott Wise <swise8@gatech.edu>
2017-10-01 23:52:41 -0700

    Add basic unit test for render

--- react/src/App.test.js
+++ react/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {MasterRank} from './containers/MasterRank.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MasterRank />, div);
 });
