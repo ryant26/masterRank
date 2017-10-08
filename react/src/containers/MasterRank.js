ef79efbfcdd933240719780864e28fc915f7a410
Scott Wise <swise8@gatech.edu>
2017-10-08 13:08:48 -0700

    Add eslinting, remove unecessary packages, fix linting errors

--- react/src/containers/MasterRank.js
+++ react/src/containers/MasterRank.js
@@ -2,17 +2,18 @@ import React, {
   Component
 } from 'react';
 import Helmet from 'react-helmet';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import * as HeroActions from '../actions/hero.js';
+// import * as HeroActions from '../actions/hero.js';
 import HeroSelector from '../components/HeroSelector.js'
 import HeroCard from '../components/HeroCard.js';
 
 export class MasterRank extends Component {
   render() {
-    const { dispatch, heroes = []} = this.props;
-    const addHero = bindActionCreators(HeroActions.addHero, dispatch)
+    const {heroes = []} = this.props;
+    // add dispatch to object above for bind
+    // const addHero = bindActionCreators(HeroActions.addHero, dispatch)
 
     const heroCardComponents = heroes.map(hero => {
       return <HeroCard hero={hero} key={hero.name} />
