fd4b4d16dbeec130af5b3ecbd781a2625d3e31a7
ryant26 <ryant26@gmail.com>
2017-10-15 21:43:05 -0600

    [Socket Server] Filled in mock authenticator. Closes #16

--- socketServer/src/mockingUtilities/mockDependencies.js
+++ socketServer/src/mockingUtilities/mockDependencies.js
@@ -1,9 +1,12 @@
 const ow = require('../apiClients/PlayerClient');
+const authentication = require('../apiClients/authenticationClient');
+const jwt = require('jsonwebtoken');
 const environment = process.env.NODE_ENV;
 const mockedEnvrionments = ['unitTest', 'develop', 'multiNodeTest'];
 
 let owPlayerApi;
 let owHeroApi;
+let authApi;
 
 let out  = {
     mockPlayerApi: function() {
@@ -37,18 +40,27 @@ let out  = {
         };
     },
 
+    mockAuthenticationApi: function() {
+        authApi = authentication.authenticate;
+        authentication.authenticate = function (tokenString) {
+            return Promise.resolve(jwt.decode(tokenString));
+        };
+    },
+
     mockAllDependenciesForEnvironment: function() {
         if(mockedEnvrionments.find((element) => {
             return element === environment;
         })) {
             out.mockPlayerApi();
             out.mockHeroApi();
+            out.mockAuthenticationApi();
         }
     },
 
     restorePlayerApi: function() {
         ow.getPlayerRank = owPlayerApi;
         ow.getHeroStats = owHeroApi;
+        authentication.authenticate = authApi;
     }
 };
 
---
+++ socketServer/src/validators/authenticationValidator.js
@@ -0,0 +1,17 @@
+const logger = require('winston');
+const SocketError = require('./exceptions/SocketError');
+const exceptions = require('./exceptions/exceptions');
+const stringValidator = require('./stringValidators').runAllValidators;
+
+let validateToken = function(token) {
+    if(stringValidator(token)) {
+        return true;
+    }
+
+    logger.error(`Recieved malformed token for authentication: ${token}`);
+    throw new SocketError(exceptions.malformedToken, 'token', token);
+};
+
+module.exports = {
+    validateToken
+};
