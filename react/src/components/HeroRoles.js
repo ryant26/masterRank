2f9e47111cc39e56d500a5ad6f71aad1869851b2
Scott Wise <swise8@gatech.edu>
2017-10-21 23:38:22 -0700

    Add Initial Hero Roles card

---
+++ react/src/components/HeroRoles.js
@@ -0,0 +1,81 @@
+import React, {
+    Component
+} from 'react';
+
+import { connect } from 'react-redux';
+import HeroCard from './HeroCard.js';
+import { names } from '../resources/allHeroNames.js';
+
+export class HeroRoles extends Component {
+
+    render() {
+
+        const offenseChars = ['doomfist', 'genji', 'mccree', 'pharah', 'reaper', 'soldier76', 'sombra', 'tracer'];
+        const defenseChars = ['bastion', 'hanzo', 'junkrat', 'mei', 'torbjorn', 'widowmaker'];
+        const tankChars = ['dva', 'orisa', 'reinhardt', 'roadhog', 'winston', 'zarya']; 
+        const supportChars = ['ana', 'lucio', 'mercy', 'symmetra', 'zenyatta']; 
+        const roles = {
+            offense: offenseChars,
+            defense: defenseChars,
+            tank: tankChars,
+            support: supportChars
+        };
+
+        const roleCardStyle = {
+            'display':'flex',
+            'borderStyle':'solid',
+            'padding':'5px 20px',
+            'justifyContent':'center'
+        };
+
+        const cardContainerStyle = {
+            'display':'flex',
+            'flexDirection':'column',
+            'borderStyle':'solid',
+            'padding':'5px 20px'
+        };
+
+        const {heroes = []} = this.props;
+        const role = this.props.role;  
+        
+        const hasRole = function(obj, heroName) {
+            let nms = names;
+            let roles = Object.keys(obj);
+            let returnVal = false;
+
+            roles.map((roleage) => {
+                obj[roleage].map((hero) => {
+                    if (heroName.name === hero && roleage === role) {
+                        returnVal = true;
+                    }
+                });
+            });
+
+            return returnVal;
+        }
+        
+        const heroCardComponents = heroes.map(hero => {
+            if (hasRole(roles, hero)) {
+                return <HeroCard hero={hero} key={Math.random().toString()} />
+            }
+        });
+
+        return (
+            <div>
+                <div className="RoleCard" style={roleCardStyle}>
+                    {role}
+                </div>
+                <div className="CardContainer" style={cardContainerStyle}>
+                    { heroCardComponents }
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => (
+    {
+      heroes: state.heroes
+    }
+);
+export default connect(mapStateToProps)(HeroRoles);
