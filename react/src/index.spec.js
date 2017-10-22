95f91dc8dbbe22734e4cf7778e40a34eb3902c07
Scott Wise <swise8@gatech.edu>
2017-10-22 01:10:54 -0700

    Add Provider to unit test to provide access to redux store

--- react/src/index.spec.js
+++ react/src/index.spec.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { FireTeam } from './containers/FireTeam.js';
+import HeroReducer from './reducers/HeroReducer.js';
 
 it('renders without crashing', () => {
+  const store = createStore(
+    HeroReducer
+  );
   const div = document.createElement('div');
-  ReactDOM.render(<FireTeam />, div);
+  ReactDOM.render(<Provider store={store}>
+                  <FireTeam />
+                  </Provider>, div);
 });
