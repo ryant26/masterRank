7f02c864829336ecb86353bcd28d53d40c6a3de5
ryant26 <ryant26@gmail.com>
2017-11-01 15:36:09 -0600

    Add runExternal for Mongo and Redis

--- runAll/runAll.js
+++ runAll/runAll.js
@@ -1,107 +1,22 @@
-const shell = require('shelljs');
 const logger = require('winston');
-const spawn = require('child_process').spawn;
+const runUtils = require('./src/runningUtilities');
 
 // Directories
 const heroApiRoot = __dirname + '/../heroApiServer';
 const socketServerRoot = __dirname + '/../socketServer';
 const landingPageRoot = __dirname + '/../landingPage';
 
-let processes = [];
-
-
-let checkExist = function(command) {
-    if (!shell.which(command)) {
-        logger.error(`Needs ${command} to exist`);
-        shell.exit(1);
-    }
-};
-
-let startProcess = function(command, startString) {
-    let process = shell.exec(command, {async: true});
-    return new Promise((resolve, reject) => {
-        process.stdout.on('data', (data) => {
-            logger.info(data);
-            if (startString) {
-                let stringForm = data.toString();
-                if (stringForm.includes(startString)) {
-                    resolve(process);
-                }
-            } else {
-                resolve(process);
-            }
-        });
-
-        process.stderr.on('data', (data) => {
-            logger.error(`${data}`);
-            if (!data.toString().includes('DeprecationWarning')) {
-                reject();
-            }
-        });
-    });
-};
-
-let startNodeProcess = function(cwd, filePath, env, startupString) {
-    return new Promise((resolve, reject) => {
-        let child = spawn('node', [filePath], {cwd, env: Object.assign({}, process.env, {NODE_ENV: env})});
-        processes.push(child);
-
-        child.stdout.on('data', (data) => {
-            logger.info(`${data}`);
-            if (startupString) {
-                let stringForm = data.toString();
-                if (stringForm.includes(startupString)) {
-                    resolve(child);
-                }
-            }
-        });
-
-        child.on('error', (err) => {
-            logger.error(err);
-            child.kill('SIGKILL');
-        });
-
-
-        child.stderr.on('data', (data) => {
-            logger.error(`${data}`);
-            if (!data.toString().includes('DeprecationWarning')) {
-                reject();
-            }
-        });
-
-        if(!startupString) {
-            resolve(child);
-        }
-    });
-};
-
-let killAllNodeProcesses = function() {
-    let out = [];
-
-    processes.forEach((proc) => {
-        out.push(new Promise((resolve) => {
-            proc.on('close', () => {
-                resolve();
-            });
-
-            proc.kill('SIGKILL');
-        }));
-    });
-
-    return Promise.all(out);
-};
-
 // Start Dependencies
-checkExist('mongod');
-checkExist('redis-server');
+runUtils.checkExist('mongod');
+runUtils.checkExist('redis-server');
 
-startProcess('mongod', 'port 27017').then(() => {
-    return startProcess('redis-server', 'port 6379');
+runUtils.startProcess('mongod', 'port 27017').then(() => {
+    return runUtils.startProcess('redis-server', 'port 6379');
 }).then(() => {
-    let proxy = startNodeProcess(heroApiRoot, 'devTools/proxy.js', 'develop');
-    let heroApiServer = startNodeProcess(heroApiRoot, 'src/app.js', 'develop', 'port 3001');
-    let socketServer = startNodeProcess(socketServerRoot, 'src/app.js', 'develop', 'port 3002');
-    let landingPage = startNodeProcess(landingPageRoot, 'fileserver.js', 'develop', 'port 3003');
+    let proxy = runUtils.startNodeProcess(heroApiRoot, 'devTools/proxy.js', 'develop');
+    let heroApiServer = runUtils.startNodeProcess(heroApiRoot, 'src/app.js', 'develop', 'port 3001');
+    let socketServer = runUtils.startNodeProcess(socketServerRoot, 'src/app.js', 'develop', 'port 3002');
+    let landingPage = runUtils.startNodeProcess(landingPageRoot, 'fileserver.js', 'develop', 'port 3003');
 
     return Promise.all([proxy, heroApiServer, socketServer, landingPage]);
 }).then(() => {
@@ -116,7 +31,7 @@ startProcess('mongod', 'port 27017').then(() => {
 }).catch(err => {
     logger.error('There was a problem starting something...');
     if (err) logger.error(err);
-    killAllNodeProcesses().then(() => {
+    runUtils.killAllNodeProcesses().then(() => {
         process.exit(1);
     });
 });
