76ef0b8c4528b0a20b72864cc0c7331784c4f29a
Scott Wise <swise8@gatech.edu>
2017-11-03 11:39:30 -0700

    Add basic unit test for SelectorButton Component

---
+++ react/src/components/SelectorButton.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import SelectorButton from './SelectorButton';
+import HeroImage from './HeroImage';
+
+describe('SelectorButton Component',()=> {
+    it('should render without exploding', () => {
+        const wrapper = mount(
+            <SelectorButton heroName="orisa"/>
+        );
+
+        const SelectorButtonComponent = wrapper.find(SelectorButton);
+        const HeroImageComponent = SelectorButtonComponent.find(HeroImage);
+
+        expect(SelectorButtonComponent.length).toBeTruthy();
+        expect(HeroImageComponent.length).toBeTruthy();
+    });
+});
