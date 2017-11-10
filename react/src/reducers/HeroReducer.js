8a0cce5c3931c23ed26d7e7dd1a3890edb97f9a8
Scott Wise <swise8@gatech.edu>
2017-11-10 14:14:15 -0800

    Change add hero reducer to simply add a single object with no wrapper hero object

--- react/src/reducers/HeroReducer.js
+++ react/src/reducers/HeroReducer.js
@@ -5,13 +5,14 @@ const initialState = {
   heroes: HEROES
 };
 
+
 export default function HeroReducer(state=initialState, action) {
   switch(action.type) {
     case HeroActionTypes.ADD_HERO:
       return {
         heroes: [
           ...state.heroes,
-          { hero: action.hero }
+          action.hero
         ]
       };
     default:
