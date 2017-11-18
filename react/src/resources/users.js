59ca2f8926d8e65c2aca7df497a6889d7d9a59c7
Scott Wise <swise8@gatech.edu>
2017-11-18 12:17:56 -0800

    Include list of users for proper render for now

--- react/src/resources/users.js
+++ react/src/resources/users.js
@@ -1,4 +1,4 @@
-const USERS = [
+export const users = [
     {
         "_id": 12,
         "platformDisplayName": "PwNShoPP#1662",
@@ -8,17 +8,5 @@ const USERS = [
         "portrait": "https://d1u1mce87gyfbn.cloudfront.net/game/unlocks/0x025000000000115A.png",
         "region": "us",
         "skillRating": 2450
-    },
-    {
-        "_id": 12,
-        "platformDisplayName": "wisesm0#1147",
-        "lastUpdated": "2017-10-15T01:43:36.459Z",
-        "platform": "pc",
-        "level": 155,
-        "portrait": "https://d1u1mce87gyfbn.cloudfront.net/game/unlocks/0x025000000000115A.png",
-        "region": "us",
-        "skillRating": 2450
     }
 ];
-
-export default USERS;
