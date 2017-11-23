0cf8a09a53f57a42600c9b9e381d31d411fc46a5
wisesm0 <dyadicofficial@gmail.com>
2017-11-22 16:02:35 -0800

    Frontend/feature/76 player card component (#116)
    * Add Initial Player Card
    
    * Fix style of player card
    
    * Remove unnecssary space from HeroImage test
    
    * Fix linting issues and proptype declarations to HeroCard
    
    * Pass user as prop to Player card and add initial unit test
    
    * Include list of users for proper render for now
    
    * Include Fireteam container to demo rendering
    
    * Fix Player card unit test
    
    * Fix styling on player card
    
    * Fix style nesting
    
    * Make player card a stateless functional component

---
+++ react/src/components/PlayerCard/PlayerCard.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import PlayerCard from './PlayerCard';
+import * as users from '../../resources/users';
+
+describe('Player Card Component', () => {
+    it('should render without exploding', () => {
+        const wrapper = mount(
+            <PlayerCard user={users.users[0]}/>
+        );
+
+        const PlayerCardComponent = wrapper.find(PlayerCard);
+        expect(PlayerCardComponent).toBeTruthy();
+        expect(JSON.stringify(PlayerCardComponent.props().user)).toBe(
+            JSON.stringify({
+                "_id": 12,
+                "platformDisplayName": "PwNShoPP#1662",
+                "lastUpdated": "2017-10-15T01:43:36.459Z",
+                "platform": "pc",
+                "level": 155,
+                "portrait": "https://d1u1mce87gyfbn.cloudfront.net/game/unlocks/0x025000000000115A.png",
+                "region": "us",
+                "skillRating": 2450
+            })
+        );
+    });
+});
