0f2e53d6691597ebd5f5360fdaff78bb5b6ec810
ryant26 <ryant26@gmail.com>
2018-01-10 12:28:21 -0700

    Initial styling of hero roles container

---
+++ react/src/components/ContentArea/HeroSelector/HeroButton/HeroButton.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import SelectorButton from './HeroButton';
+import HeroImage from '../../../HeroImage/HeroImage';
+
+describe('HeroButton Component',()=> {
+    it('should render without exploding', () => {
+        const wrapper = mount(
+            <SelectorButton heroName="orisa" selected={false}/>
+        );
+
+        const SelectorButtonComponent = wrapper.find(SelectorButton);
+        const HeroImageComponent = SelectorButtonComponent.find(HeroImage);
+
+        expect(SelectorButtonComponent.length).toBeTruthy();
+        expect(HeroImageComponent.length).toBeTruthy();
+    });
+});
--- react/src/reducers/HeroReducer.js
+++ react/src/reducers/HeroReducer.js
@@ -1,8 +1,22 @@
 import * as HeroActionTypes from '../actiontypes/hero';
 import {arrayHasDuplicate} from "./reducerUtilities";
+import MockHeroes from '../resources/heroes';
+const names = require('../../../shared/allHeroNames').names;
 
+let initialState = [];
 
-export default function HeroReducer(state=[], action) {
+const getRandomHeroName = function() {
+    let randNum = Math.floor(Math.random() * names.length);
+    return names[randNum];
+};
+
+for (let i = 0; i < 300; i++) {
+    let hero = Object.assign({}, MockHeroes[0]);
+    hero.heroName = getRandomHeroName();
+    initialState.push(hero);
+}
+
+export default function HeroReducer(state=initialState, action) {
   switch(action.type) {
       case HeroActionTypes.ADD_HERO: {
           let out = state;
