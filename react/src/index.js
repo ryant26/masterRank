1490bb5e63ae4d2c6d76c2a73135ce1322bdb326
Alex Swizzl <alex.swizzl@gmail.com>
2018-01-10 21:47:46 +0900

    add router for login vs homepage

--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 import store from "./model/store";
 import './index.css';
+
 require('./stylesheets/main.scss');
 
 import FireTeam from './containers/FireTeam';
+import LoginPage from './containers/LoginPage';
 
 render(
   <Provider store={store}>
-    <FireTeam />
+    <Router>
+        <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/" component={FireTeam} />
+        </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
