eb1620c9fd4295a988350591b5e801b5018470b2
Alex Swizzl <alex.swizzl@gmail.com>
2018-01-12 17:41:12 +0900

    refactor improve readability

--- react/src/containers/LoginPage.js
+++ react/src/containers/LoginPage.js
@@ -7,14 +7,15 @@ import PlayerCard from '../components/Sidebar/PlayerCard/PlayerCard';
 //TODO: Remove before submitting PR
 import * as users from '../resources/users';
 
-const urlForPlayerInfo = battletag =>
-  `http://localhost:3003/api/players/search?platformDisplayName=${battletag}`
+// TODO: Are we going to set up a proxy or does the 3003 port have to be hardcoded?
+const urlForPlayerSearch = displayName =>
+  `http://localhost:3003/api/players/search?platformDisplayName=${displayName}`
 
 export default class LoginPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            battletag: '',
+            displayName: '',
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,56 +23,58 @@ export default class LoginPage extends Component {
     }
 
     handleChange(event) {
-        this.setState({battletag: event.target.value});
+        this.setState({displayName: event.target.value});
     }
 
     handleSubmit(event) {
         //Query player using /api/player/search
-//        fetch(urlForPlayerInfo(this.state.battletag))
-//          .then(response => {
-//            if (!response.ok) {
-//              throw Error("Network request failed")
-//            }
-//
-//            return response
-//          })
-//          .then(response => response.json())
-//          .then(response => {
-//            this.setState({
-//              user: response
-//            })
-//          })
-//        Check if valid use is retured
+        fetch(urlForPlayerSearch(this.state.displayName))
+          .then(response => {
+            if (!response.ok) {
+              throw Error("Network request failed")
+            }
+
+            return response
+          })
+          .then(response => response.json())
+          .then(response => {
+            this.setState({
+              user: response
+            })
+          })
+
+//      Check if valid use is returned
         let user = users.users[0];
-        user.platformDisplayName = this.state.battletag;
+        user.platformDisplayName = this.state.displayName;
         this.setState({
             user: user
         });
 
         //redirect to homepage
-        this.props.history.push("/");
+//        this.props.history.push("/");
     }
 
     render() {
-        if(this.state.user){
-            return (
-                <PlayerCard user={this.state.user}/>
-            );
-        } else {
-            return (
-                <div className="body">
-                  <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        value={this.state.battletag}
-                        onChange={this.handleChange}
-                        placeholder="Enter Full Battletag, PSN, or Xbox Gamertag..."
-                        className="input-primary"
-                    />
-                    <input type="submit" value="Search" className="button-primary" />
-                  </form>
-                </div>
-            );
-        }
+        return (
+            <div className="container-md flex">
+            { this.state.user
+                ? (<PlayerCard user={this.state.user}/>)
+                : (<div className="input-component skew">
+                    <form onSubmit={this.handleSubmit} className="validate">
+                        <div className="input-container">
+                            <input
+                                type="text"
+                                value={this.state.displayName}
+                                onChange={this.handleChange}
+                                placeholder="Enter Full Battletag, PSN, or Xbox Gamertag..."
+                                className="unskew stretch"
+                            />
+                        </div>
+                        <input type="submit" value="Search" className="input-button" />
+                    </form>
+                 </div>)
+             }
+             </div>
+        );
     }
 }
