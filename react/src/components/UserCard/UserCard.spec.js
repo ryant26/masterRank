9d465a27a8ceb05986b107cba950378c27f99c82
Alex Swizzl <alex.swizzl@gmail.com>
2018-01-16 16:08:38 +0900

    Update defualt user to have no skill rating, and add a test to test that skill rating is rendered when present.

--- react/src/components/UserCard/UserCard.spec.js
+++ react/src/components/UserCard/UserCard.spec.js
@@ -12,16 +12,20 @@ describe('User Card Component', () => {
         const UserCardComponent = wrapper.find(UserCard);
         expect(UserCardComponent).toBeTruthy();
         expect(JSON.stringify(UserCardComponent.props().user)).toBe(
-            JSON.stringify({
-                "_id": 12,
-                "platformDisplayName": "PwNShoPP#1662",
-                "lastUpdated": "2017-10-15T01:43:36.459Z",
-                "platform": "pc",
-                "level": 155,
-                "portrait": "https://d1u1mce87gyfbn.cloudfront.net/game/unlocks/0x025000000000115A.png",
-                "region": "us",
-                "skillRating": 2450
-            })
+            JSON.stringify(users.users[0])
+        );
+    });
+
+    it('should render skill rating when present', () => {
+        let userWithSkillRating = users.users[0]['skillRating'] = 2450;
+        const wrapper = mount(
+            <UserCard user={userWithSkillRating}/>
+        );
+
+        let UserCardComponent = wrapper.find(UserCard);
+        expect(UserCardComponent).toBeTruthy();
+        expect(JSON.stringify(UserCardComponent.props().user)).toBe(
+            JSON.stringify(userWithSkillRating)
         );
     });
 });
