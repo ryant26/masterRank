0bd585596df784b846198747f9a17420a64d8520
Alex Swizzl <alex.swizzl@gmail.com>
2018-01-29 19:03:17 +0900

    Change to functional component

--- react/src/components/UserSelector/UserSelector.js
+++ react/src/components/UserSelector/UserSelector.js
@@ -1,38 +1,30 @@
-import React, {
-    Component
-} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import UserCard from '../UserCard/UserCard';
 
-export default class UserSelector extends Component {
+const UserSelector = ({users}) => {
 
-    constructor(props) {
-        super(props);
-
-        this.onClick = this.onClick.bind(this);
+    function onClick() {
+        window.location.assign(redirectUrl());
     }
 
-    onClick() {
-        window.location.assign(this.redirectUrl());
-    }
-
-    redirectUrl() {
+    function redirectUrl() {
         //TODO: Decide what to do about region and unhard code "us" here
         return `/auth/bnet/callback?region=us`;
     }
 
-    render() {
-        return (
-            <div className="UserSelector">
-                { this.props.users.map((user, i) =>
-                    <UserCard user={user} key={i} onClick={this.onClick} />
-                )}
-            </div>
-        );
-    }
-}
+    return (
+        <div className="UserSelector">
+            { users.map((user, i) =>
+                <UserCard user={user} key={i} onClick={onClick} />
+            )}
+        </div>
+    );
+};
 
 UserSelector.propTypes = {
   users: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
+
+export default UserSelector;
