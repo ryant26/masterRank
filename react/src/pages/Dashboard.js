d7b48d79578c6d5db0563d357d0bc7296a3edf32
Alex Swizzl <alex.swizzl@gmail.com>
2018-02-03 19:48:16 +0900

    fix linter

--- react/src/pages/Dashboard.js
+++ react/src/pages/Dashboard.js
@@ -1,7 +1,6 @@
 import React, {
   Component
 } from 'react';
-import PropTypes from 'prop-types';
 
 import Sidebar from '../components/Sidebar/Sidebar';
 import ContentArea from '../components/ContentArea/ContentArea';
