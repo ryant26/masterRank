8afb1f0b87acb56b56427cb0bd537b93e2992744
Ryan Thornhill <ryan.thornhill@ge.com>
2018-02-10 00:25:38 -0700

    Make region/platform selector props consistent

--- react/src/components/Login/RegionSelection/RegionSelection.js
+++ react/src/components/Login/RegionSelection/RegionSelection.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RegionSelection = ({selectedRegion, handleOptionChange}) => {
+const RegionSelection = ({onChange, selectedRegion}) => {
     const us = 'us';
     const apac = 'apac';
     const eu = 'eu';
 
     return(
         <form className="RegionSelection toggle-buttons">
-            <input type="radio" id="region-1" name="region" value={us} checked={selectedRegion === us} onChange={handleOptionChange}/>
+            <input type="radio" id="region-1" name="region" value={us} checked={selectedRegion === us} onChange={onChange}/>
             <label htmlFor="region-1"><div className="button-content">NA</div></label>
-            <input type="radio" id="region-2" name="region" value={apac} checked={selectedRegion === apac} onChange={handleOptionChange}/>
+            <input type="radio" id="region-2" name="region" value={apac} checked={selectedRegion === apac} onChange={onChange}/>
             <label htmlFor="region-2"><div className="button-content">APAC</div></label>
-            <input type="radio" id="region-3" name="region" value={eu} checked={selectedRegion === eu} onChange={handleOptionChange}/>
+            <input type="radio" id="region-3" name="region" value={eu} checked={selectedRegion === eu} onChange={onChange}/>
             <label htmlFor="region-3"><div className="button-content">EU</div></label>
         </form>
     );
 };
 
 RegionSelection.propTypes = {
-    handleOptionChange: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
     selectedRegion: PropTypes.string.isRequired
 };
 
