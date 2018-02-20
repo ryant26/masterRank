5b78ae6c008b7183647d494783b524152a6850a4
Alex Swizzl <alex.swizzl@gmail.com>
2018-02-20 23:54:13 +0900

    closes #268

--- react/src/components/Stats/HeroImages/HeroImages.spec.js
+++ react/src/components/Stats/HeroImages/HeroImages.spec.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+
 import HeroImages from './HeroImages';
+import HeroImage from '../../HeroImage/HeroImage';
 
 const names = require('../../../../../shared/libs/allHeroNames').names;
 
-describe('HeroCard Component',()=> {
-    it('should render without exploding', () => {
-        const wrapper = mount(
-            <HeroImages heroNames={[names[0], names[1], names[2]]}/>
-        );
 
-        const HeroCardComponent = wrapper.find(HeroImages);
-        expect(HeroCardComponent).toBeTruthy();
+describe('HeroCard Component',()=> {
+    const heroNames = [names[0], names[1], names[2]];
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(
+            <HeroImages heroNames={heroNames}/>
+        );
+    });
+
+    it('should render without exploding', () => {
+        expect(wrapper.find(HeroImages)).toBeTruthy();
+    });
+
+    it('should have a heroImage for each heroName', () => {
+        expect(wrapper.find(HeroImage)).toHaveLength(heroNames.length);
+    });
+
+    it("should render heroImages in order", () => {
+        heroNames.forEach((heroName, i) => {
+            expect(wrapper.find(HeroImage).at(i).props().heroName).toBe(heroName);
+        });
     });
 
     it('should match the snapshot', () => {
         let component = renderer.create(
-            <HeroImages heroNames={[names[0], names[1], names[2]]}/>
+            <HeroImages heroNames={heroNames}/>
         );
 
         let tree = component.toJSON();
