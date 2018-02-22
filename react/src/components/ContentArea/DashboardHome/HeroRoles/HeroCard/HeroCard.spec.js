c7e6aaf66b3d51e78de58263b19aacca5292ba5d
aswizzl <alex.swizzl@gmail.com>
2018-02-22 15:39:45 +0900

    Add stat-less hero cards, hookup invite button on stats modal, add invite notification

--- react/src/components/ContentArea/DashboardHome/HeroRoles/HeroCard/HeroCard.spec.js
+++ react/src/components/ContentArea/DashboardHome/HeroRoles/HeroCard/HeroCard.spec.js
@@ -4,9 +4,16 @@ import configureStore from 'redux-mock-store';
 
 import Model from '../../../../../model/model';
 import HeroCard from './HeroCard';
+import Modal from '../../../../Modal/Modal';
+import UserStatsContainer from '../../../../Stats/UserStatsContainer';
+import { inviteNotification } from '../../../../Notifications/Notifications';
+jest.mock('../../../../Notifications/Notifications');
+
 import heroes from '../../../../../resources/heroes';
 import { users } from '../../../../../resources/users';
 import groupInvites from '../../../../../resources/groupInvites';
+
+
 
 const mockStore = configureStore();
 const getHeroCardComponent = (user, hero, group) => {
@@ -89,12 +96,48 @@ describe('HeroCard Component',()=> {
         expect(HeroCardComponent.find('.invitable')).toHaveLength(0);
     });
 
-    it('should call model invite player when when the plus-container div is clicked', () => {
-        Model.inviteUserToGroup = jest.fn();
-        HeroCardComponent.find('.plus-container').simulate('click');
-        expect(Model.inviteUserToGroup).toHaveBeenCalledWith({
-            platformDisplayName: hero.platformDisplayName,
-            heroName: hero.heroName
+    describe("when the heroCard's plus icon is clicked", () => {
+
+        beforeEach(() => {
+            Model.inviteUserToGroup = jest.fn();
+            HeroCardComponent.find('.plus-container').simulate('click');
+        });
+
+        it('should call model invite player', () => {
+            expect(Model.inviteUserToGroup).toHaveBeenCalledWith({
+                platformDisplayName: hero.platformDisplayName,
+                heroName: hero.heroName
+            });
+        });
+
+        it('should send invite notification with correct display name', () => {
+            expect(inviteNotification).toHaveBeenCalledWith(hero.platformDisplayName);
         });
     });
+
+    it('should render Modal', () => {
+        expect(HeroCardComponent.find(Modal)).toHaveLength(1);
+    });
+
+    describe('UserStatsContainer', () => {
+
+        it('should render', () => {
+            expect(HeroCardComponent.find(UserStatsContainer)).toHaveLength(1);
+        });
+
+        it('should pass hero to props', () => {
+            expect(HeroCardComponent.find(UserStatsContainer).props().hero).toBe(hero);
+        });
+
+        it('should pass invitable to props', () => {
+            expect(HeroCardComponent.find('.invitable')).toHaveLength(0);
+            expect(HeroCardComponent.find(UserStatsContainer).props().invitable).toBe(false);
+        });
+
+        it('should pass toggleModal to props', () => {
+            expect(HeroCardComponent.find(UserStatsContainer).props().toggleModal)
+                .toBe(HeroCardComponent.instance().toggleModal);
+        });
+    });
+
 });
--- react/src/components/Notifications/Notifications.spec.js
+++ react/src/components/Notifications/Notifications.spec.js
@@ -8,6 +8,7 @@ import groupInvites from '../../resources/groupInvites';
 
 import {
     joinGroupNotification,
+    inviteNotification,
     errorNotification,
     disconnectedNotification
 } from './Notifications';
@@ -18,15 +19,16 @@ describe('Notifications', () => {
     describe('joinGroupNotification', () => {
         it('should call toast with correct props', () => {
             const displayName = groupInvites[0].leader.platformDisplayName.replace(/#.*/,"");
-            const type = "success";
+            const icon="fa fa-thumbs-up";
             const title = <span>You've joined <b>{displayName}'s</b> group!</span>;
             const message = <span>Add <b>{displayName}</b> while you wait for other players to join</span>;
+            const type = "success";
 
             joinGroupNotification(displayName);
 
             expect(toast).toHaveBeenCalledWith(
                 <Notification
-                    icon="fa fa-thumbs-up"
+                    icon={icon}
                     title={title}
                     message={message}
                     type={type}
@@ -40,17 +42,44 @@ describe('Notifications', () => {
         });
     });
 
-   describe('errorNotification with correct props', () => {
+    describe('inviteNotification with correct props', () => {
         it('should call toast ', () => {
-            const type = "error";
+            const inviteeDisplayName = groupInvites[0].leader.platformDisplayName;
+            const icon="fa fa-user-plus";
+            const title = `Invite sent to ${inviteeDisplayName}`;
+            const message = 'You should see an invite timeout at the bottom left side of your screen';
+            const type = "success";
+
+            inviteNotification(inviteeDisplayName);
+
+            expect(toast).toHaveBeenCalledWith(
+                <Notification
+                    icon={icon}
+                    title={title}
+                    message={message}
+                    type={type}
+                />,
+                {
+                    autoClose: 30000,
+                    className: "NotificationContainer",
+                    progressClassName:`${type}-progress`
+                }
+            );
+        });
+    });
+
+    describe('errorNotification with correct props', () => {
+        it('should call toast when error message is defined', () => {
+            const icon="fa fa-exclamation";
             const title = "Something went wrong!";
             const errorMessage = 'Error adding hero "tracer", please try again or refresh page';
+            const type = "error";
 
             errorNotification(errorMessage);
 
             expect(toast).toHaveBeenCalledWith(
                 <Notification
-                   icon="fa fa-exclamation"
+                   icon={icon}
                    title={title}
                    message={errorMessage}
                    type={type}
@@ -62,19 +91,43 @@ describe('Notifications', () => {
                 }
             );
         });
-   });
 
-   describe('disconnectedNotification with correct props', () => {
+        it('should call toast when error message is null', () => {
+            const icon="fa fa-exclamation";
+            const title = "Something went wrong!";
+            const errorMessage = null;
+            const type = "error";
+
+            errorNotification(errorMessage);
+
+            expect(toast).toHaveBeenCalledWith(
+                <Notification
+                   icon={icon}
+                   title={title}
+                   message="please refresh the page"
+                   type={type}
+                />,
+                {
+                   autoClose: 30000,
+                   className: "NotificationContainer",
+                   progressClassName:`${type}-progress`
+                }
+            );
+        });
+    });
+
+    describe('disconnectedNotification with correct props', () => {
        it('should call toast ', () => {
-          const type = "warning";
-          const message = "Please wait while we try to reconnect...";
+          const icon="fa fa-plug";
           const title = "You have been disconnected";
+          const message = "Please wait while we try to reconnect...";
+          const type = "warning";
 
           disconnectedNotification();
 
           expect(toast).toHaveBeenCalledWith(
               <Notification
-                  icon="fa fa-plug"
+                  icon={icon}
                   title={title}
                   message={message}
                   type={type}
@@ -86,5 +139,5 @@ describe('Notifications', () => {
               }
           );
        });
-  });
+    });
 });
