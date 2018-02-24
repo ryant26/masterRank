8f0f4bcb6ea4acc6176e859e0e364d0587604451
Ryan Thornhill <ryan.thornhill@ge.com>
2018-02-23 19:17:14 -0800

    Add alpha tag to title

--- react/src/components/Sidebar/Title/Title.js
+++ react/src/components/Sidebar/Title/Title.js
@@ -3,16 +3,17 @@ import {Link} from 'react-router-dom';
 import {home} from "../../Routes/links";
 
 const Title = () => {
-  return (
-    <div className="Title">
-      <Link to={home} style={{ textDecoration: 'none' }}>
-        <div className="flex align-center">
-          <img src={require('../../../assets/logo-icon.svg')} alt="logo-icon"/>
-          <h1>FIRETEAM.GG</h1>
+    return (
+        <div className="Title">
+            <Link to={home} style={{textDecoration: 'none'}}>
+                <div className="flex align-center justify-between">
+                    <img src={require('../../../assets/logo-icon.svg')} alt="logo-icon"/>
+                    <div className="fireteam">FIRETEAM.GG</div>
+                    <div className="alpha-tag">Alpha</div>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  );
+    );
 };
 
 export default Title;
