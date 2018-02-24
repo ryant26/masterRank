dfb54aa3049793d0402aeb4440a29d1b4992fc9f
Ryan Thornhill <ryan.thornhill@ge.com>
2018-02-23 19:50:19 -0800

    Add beta card to content area

---
+++ react/src/components/ContentArea/DashboardHome/Beta Card/BetaCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BetaCard = () => {
+    return (
+        <div className="BetaCard flex justify-center align-center">
+            Welcome to Fireteam.gg, thanks for participating in our Alpha release!
+        </div>
+    );
+};
+
+export default BetaCard;
