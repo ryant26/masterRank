a8fa9a8c352c8d9fbead924853d9a99edf1a25f4
Ryan Thornhill <ryan.thornhill@ge.com>
2018-02-23 19:50:19 -0800

    Add beta card to content area

--- react/src/components/ContentArea/DashboardHome/DashboardHome.js
+++ react/src/components/ContentArea/DashboardHome/DashboardHome.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeroFilters from './HeroSelector/HeroSelectorCard';
 import HeroRoles from './HeroRoles/HeroRolesContainer';
-
+import BetaCard from './Beta Card/BetaCard';
 
 const DashboardHome = () => {
     return (
         <div className="DashboardHome flex flex-column">
+            <BetaCard/>
             <HeroFilters/>
             <HeroRoles/>
         </div>
