3e37fd37fcd5490632aa5f60745a029b1a5f7206
ryant26 <ryant26@gmail.com>
2018-02-25 01:22:14 -0700

    Rename BetaCard -> AlphaCard

--- react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
+++ react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const BetaCard = () => {
+const AlphaCard = () => {
     return (
-        <div className="BetaCard flex justify-center align-center">
+        <div className="AlphaCard flex justify-center align-center">
             Welcome to Fireteam.gg, thanks for participating in our Alpha release!
         </div>
     );
 };
 
-export default BetaCard;
+export default AlphaCard;
