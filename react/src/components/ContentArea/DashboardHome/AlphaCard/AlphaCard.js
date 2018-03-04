a3b8935a87a73d8bde998a9bd97a4b5ba0386ac1
aswizzl <alex.swizzl@gmail.com>
2018-03-04 17:23:55 +0900

    Add feedback link to alpha card, use feedback.fireteam.gg

--- react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
+++ react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { feedback } from '../../../Routes/links';
+
 const AlphaCard = () => {
+    const feedbackLink = ( <a className="feedback-link" href={feedback}><b>feedback</b></a> );
+
     return (
-        <div className="AlphaCard flex justify-center align-center">
+        <div className="AlphaCard">
             Welcome to Fireteam.gg, thanks for participating in our Alpha release!
+            Please send us any {feedbackLink} regarding your experience with Fireteam.gg
         </div>
     );
 };
