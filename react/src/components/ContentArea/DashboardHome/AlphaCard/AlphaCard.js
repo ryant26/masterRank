8974b719407cdcdd8385e85720f0110717dcd761
Alex Swizzl <alex.swizzl@gmail.com>
2018-03-04 17:01:45 +0900

    remove flex

--- react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
+++ react/src/components/ContentArea/DashboardHome/AlphaCard/AlphaCard.js
@@ -6,7 +6,7 @@ const AlphaCard = () => {
     const feedbackLink = ( <a className="feedback-link" href={feedback}><b>feedback</b></a> );
 
     return (
-        <div className="AlphaCard justify-center align-center">
+        <div className="AlphaCard">
             Welcome to Fireteam.gg, thanks for participating in our Alpha release!
             Please send us any {feedbackLink} regarding your experience with Fireteam.gg
         </div>
