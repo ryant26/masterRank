14bd88ba9d641b2151cebf0a4d04771774901fe3
aswizzl <alex.swizzl@gmail.com>
2018-03-09 01:43:00 +0900

    Add Google Analytics events

---
+++ react/src/model/trackers/GoogleAnalyticTracker.js
@@ -0,0 +1,21 @@
+class GoogleAnalyticTracker {
+
+    constructor(gtag) {
+        this.gtag = gtag;
+    }
+
+    trackEvent(action) {
+        let [eventCategory, eventAction] = action.type.split('/');
+        if(process.env.NODE_ENV === 'development') {
+            console.log(`GA event= [${action.region}, ${eventCategory}, ${eventAction}]`);
+        } else {
+            this.gtag('event', eventAction, {
+              event_category: eventCategory,
+              event_label: action.region
+            });
+        }
+    }
+}
+
+export default GoogleAnalyticTracker;
+
