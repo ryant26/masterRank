c538f19915ef0c6f22ecb79a3a6718a5ea0834a4
Ryan Thornhill <ryan.thornhill@ge.com>
2018-03-15 17:48:19 -0600

    Refactor heroRolesList into SFC and tests with mockStore

--- react/src/components/ContentArea/DashboardHome/HeroRoles/HeroRolesList/HeroRolesList.spec.js
+++ react/src/components/ContentArea/DashboardHome/HeroRoles/HeroRolesList/HeroRolesList.spec.js
@@ -1,127 +1,93 @@
 import React from 'react';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import renderer from 'react-test-renderer';
-import { createRenderer } from 'react-test-renderer/shallow';
 import { shallow } from 'enzyme';
 
-import HeroRoles from './HeroRolesList';
+import HeroRolesList from './HeroRolesList';
 import HeroCard from '../HeroCard/HeroCard';
-import HeroReducer from '../../../../../reducers/HeroReducer';
-import {addHero} from "../../../../../actionCreators/heroes/hero";
 import heroes from '../../../../../resources/heroes';
 import {users} from '../../../../../resources/users';
-import groupInvites from '../../../../../resources/groupInvites';
 
-const realStore = createStore(combineReducers({heroes:HeroReducer}),{});
-const setup = propOverrides => {
-    const props = Object.assign({
-        role: 'tank',        
-        heroes: [heroes[0]],
-        user: users[0]
-    }, propOverrides);
-
-
-    const renderer = createRenderer();
-    renderer.render(<Provider store = {realStore}>
-                        <HeroRoles {...props}/>
-                    </Provider>);
-    const output = renderer.getRenderOutput();
-
-    return {
-        props: props,
-        output: output
-    };
-};
 
 describe('HeroRolesList Component', () => {
     let wrapper;
 
-    const getHeroRoles = ({heroes, role, user}) => {
+    const getHeroRoles = ({heroes, role}) => {
         return shallow(
-            <Provider store ={realStore}>
-                <HeroRoles heroes={heroes} role={role} user={user}/>
-            </Provider>
+            <HeroRolesList heroes={heroes} role={role}/>
         );
     };
 
     beforeEach(() => {
         wrapper = getHeroRoles({
             heroes: [heroes[0]],
-            role: 'tank',
-            user: users[0]
+            role: 'tank'
         });
     });
 
     it('should render without exploding', () => {
-        realStore.dispatch(addHero(heroes[0]));
-        expect(wrapper.find(HeroRoles)).toHaveLength(1);
-        expect(wrapper.dive().find(HeroCard)).toHaveLength(1);
+        expect(wrapper).toHaveLength(1);
+        expect(wrapper.find(HeroCard)).toHaveLength(1);
     });
 
-    it('should render container with one hero with correct role', () => {
-        const { output } = setup();
-        expect(output.props.role).toBe('tank');
+    it('should render multiple heroCards', () => {
+        wrapper = getHeroRoles({
+            heroes: [heroes[0], heroes[1]],
+            role: 'tank'
+        });
+
+        expect(wrapper.find(HeroCard)).toHaveLength(2);
     });
 
-    it('should render container with one hero with correct name', () => {
-        const { output } = setup();
-        expect(output.props.heroes[0].heroName).toBe('soldier76');
+    it('should render a message when there are no hero cards', () => {
+        wrapper = getHeroRoles({
+            heroes: [],
+            role: 'tank'
+        });
+
+        expect(wrapper.find('.empty-list-message > div').text()).toBe('No players online');
     });
 
     it('should render the container with hero cards sorted by winRate', () => {
-        const hero1 = heroes[0];
-        const hero2 = heroes[4];
+        const heroWithLowerWinRate = heroes[0];
+        const heroWithHigherWinRate = heroes[4];
 
-        const hero1WinRate = hero1.stats.wins / hero1.stats.gamesPlayed;
-        const hero2WinRate = hero2.stats.wins / hero2.stats.gamesPlayed;
+        const lowerWinRate = heroWithLowerWinRate.stats.wins / heroWithLowerWinRate.stats.gamesPlayed;
+        const higherWinRate = heroWithHigherWinRate.stats.wins / heroWithHigherWinRate.stats.gamesPlayed;
 
         wrapper = getHeroRoles({
-            heroes: [hero1, hero2],
+            heroes: [heroWithLowerWinRate, heroWithHigherWinRate],
             role: 'tank',
             user: users[0]
         });
 
-        expect(wrapper.dive().find(HeroCard)).toHaveLength(2);
-        expect(hero1WinRate).toBeLessThan(hero2WinRate);
-        expect(wrapper.dive().find(HeroCard).first().props().hero.heroName).toBe(hero2.heroName);
+        expect(wrapper.find(HeroCard)).toHaveLength(2);
+        expect(lowerWinRate).toBeLessThan(higherWinRate);
+        expect(wrapper.find(HeroCard).first().props().hero.heroName).toBe(heroWithHigherWinRate.heroName);
     });
 
-    describe('should match the snapshot', () => {
-        const getHeroRolesTree = (heroes) => {
-            let component = renderer.create(
-                <Provider store={store}>
-                    <HeroRoles heroes={heroes} role="tank" user={{platformDisplayName: 'myName'}}/>
-                </Provider>
-            );
+    describe('title should match the role prop', () => {
+        const testMatchingTitleAndRole = (role) => {
+            wrapper = getHeroRoles({
+                heroes: [],
+                role
+            });
 
-            return component.toJSON();
+            expect(wrapper.find('h3').text()).toBe(role);
         };
 
-        const mockStore = configureStore();
-        let store;
-
-        beforeEach(() =>{
-            store = mockStore({
-                group: groupInvites[0],
-                user: users[0]
-            });
+        it('tank', () => {
+            testMatchingTitleAndRole('tank');
         });
 
-        it('multiple heroes', () => {
-            let tree = getHeroRolesTree(heroes);
-            expect(tree).toMatchSnapshot();
+        it('support', () => {
+            testMatchingTitleAndRole('support');
         });
 
-        it('single hero', () => {
-            let tree = getHeroRolesTree([heroes[0]]);
-            expect(tree).toMatchSnapshot();
+        it('attack', () => {
+            testMatchingTitleAndRole('attack');
         });
 
-        it('no heroes', () => {
-            let tree = getHeroRolesTree([]);
-            expect(tree).toMatchSnapshot();
+        it('defense', () => {
+            testMatchingTitleAndRole('defense');
         });
     });
-}); 
+});
