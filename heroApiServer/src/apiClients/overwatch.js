cb27ab2b2dfed46bcc194241043f88ba4e5eea27
ryant26 <ryant26@gmail.com>
2018-04-30 21:30:10 -0600

    [HeroApiServer] Handle special characters in battle tags

--- heroApiServer/src/apiClients/overwatch.js
+++ heroApiServer/src/apiClients/overwatch.js
@@ -3,14 +3,14 @@ let ow = require('oversmash').default({
 });
 
 let getPlayerDetails = function(token) {
-    return ow.player(token.platformDisplayName).then((result) => {
+    return getAccountInfo(token.platformDisplayName).then((result) => {
         return result.accounts.find((element) => element.platform === token.platform
             && element.displayName === token.platformDisplayName);
     });
 };
 
 let searchForPlayer = function(token) {
-    return ow.player(token.platformDisplayName).then((result) => {
+    return getAccountInfo(token.platformDisplayName).then((result) => {
         result = result.accounts;
 
         result.forEach((account) => {
@@ -28,13 +28,17 @@ let searchForPlayer = function(token) {
     });
 };
 
+let getAccountInfo = function(platformDisplayName) {
+    return ow.player(encodeURIComponent(platformDisplayName));
+};
+
 let getPlayerStats = function(token) {
     let playerId = _normalizeId(token.platformDisplayName);
     return ow.playerStats(playerId, null, token.platform);
 };
 
 let _normalizeId = function(platformDisplayName) {
-    return platformDisplayName.replace('#', '-');
+    return encodeURIComponent(platformDisplayName.replace('#', '-'));
 };
 
 module.exports = {
