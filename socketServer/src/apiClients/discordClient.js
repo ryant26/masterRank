33f3d9f92a30db9174c63fd5047c4131901b20df
ryant26 <ryant26@gmail.com>
2018-05-27 17:34:16 -0600

    Discord API created and tested

---
+++ socketServer/src/apiClients/discordClient.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const logger = require('../services/logger').sysLogger;
+const config =require('config');
+
+// Authentication Constants
+const token = config.get('discordApi.token');
+const guildId = config.get('discordApi.guild.id');
+
+// API Constants
+const maxAgeInSec = config.get('discordApi.invite.maxAgeInSec');
+const voiceChannelType = 'voice';
+
+const getGuild = new Promise((resolve) => {
+    client.on('ready', () => {
+        logger.info('Connected to discord');
+        resolve(client.guilds.find((guild) => guild.id === guildId));
+    });
+
+    client.login(token);
+});
+
+const createVoiceChannel = function(groupId) {
+    return getGuild.then((guild) => {
+        return guild.createChannel(`group-${groupId}`, voiceChannelType);
+    });
+};
+
+const getChannel = function(id) {
+    return getAllChannels().then((channels) => {
+        return channels.find((channel) => channel.id === id);
+    });
+};
+
+const getAllChannels = function() {
+    return getGuild.then((guild) => {
+        return guild.channels;
+    });
+};
+
+const deleteChannel = function(id) {
+    return getChannel(id).then((channel) => {
+        return channel ? channel.delete() : null;
+    });
+};
+
+const getVoiceChannelInvite = function(groupId) {
+    return createVoiceChannel(groupId).then((channel) => {
+        return channel.createInvite({
+            temporary: true,
+            maxAge: maxAgeInSec
+        });
+    });
+};
+
+module.exports = {
+    createVoiceChannel: createVoiceChannel,
+    getChannel: getChannel,
+    getAllChannels: getAllChannels,
+    deleteVoiceChannel: deleteChannel,
+    getVoiceChannelInvite: getVoiceChannelInvite
+};
---
+++ socketServer/test/functionalTest/apiClients/discordClientTest.js
@@ -0,0 +1,115 @@
+const assert = require('chai').assert;
+const client = require('../../../src/apiClients/discordClient');
+
+const timeout = 20000;
+
+describe('Discord API', function() {
+    this.timeout(timeout);
+
+    afterEach(function() {
+        return client.getAllChannels().then((channels) => {
+            return Promise.all(channels.map((channel) => channel.delete()));
+        });
+    });
+
+    describe('createVoiceChannel', function() {
+        const groupId = 2;
+        
+        it('should create a channel with the group ID in the name', function() {
+            return client.createVoiceChannel(groupId).then((channel) => {
+                return client.getChannel(channel.id);
+            }).then((channel) => {
+                assert.include(channel.name, groupId);
+            });
+        });
+
+        it('should create a channel of type voice', function() {
+            return client.createVoiceChannel(groupId).then((channel) => {
+                return client.getChannel(channel.id);
+            }).then((channel) => {
+                assert.equal(channel.type, 'voice');
+            });
+        });
+    });
+
+    describe('getChannel', function() {
+        it('should return the correct channel', function() {
+            const myGroupId = 1234;
+            let createdChannel;
+
+            return client.createVoiceChannel(myGroupId).then((channel) => {
+                createdChannel = channel;
+                return client.getChannel(channel.id);
+            }).then((channel) => {
+                assert.deepEqual(createdChannel, channel);
+            });
+        });
+    });
+
+    describe('getAllChannels', function() {
+        it('should return all channels on the server', function(){
+            const channel1 = 'chan1';
+            const channel2 = 'chan2';
+            const channel3 = 'chan3';
+
+            let channelArray;
+
+            return Promise.all([
+                client.createVoiceChannel(channel1),
+                client.createVoiceChannel(channel2),
+                client.createVoiceChannel(channel3)
+            ]).then((results) => {
+                channelArray = results;
+                return client.getAllChannels();
+            }).then((results) => {
+                assert.containsAllKeys(results, channelArray.map((channel) => channel.id));
+            });
+        });
+    });
+
+    describe('deleteChannel', function() {
+        it('should delete a channel while leaving all others', function() {
+            const channel1 = 'chan1';
+            const channel2 = 'chan2';
+            const channel3 = 'chan3';
+
+            let channelArray;
+
+            return Promise.all([
+                client.createVoiceChannel(channel1),
+                client.createVoiceChannel(channel2),
+                client.createVoiceChannel(channel3)
+            ]).then((results) => {
+                channelArray = results;
+                return client.deleteVoiceChannel(results[0].id);
+            }).then(() => {
+                return client.getAllChannels();
+            }).then((results) => {
+                let containedChannels = [channelArray[1], channelArray[2]];
+                assert.containsAllKeys(results, containedChannels.map((channel) => channel.id));
+                assert.doesNotHaveAnyKeys(results, [channelArray[0].id]);
+            });
+        });
+    });
+
+    describe('getVoiceChannelInvite', function() {
+        const groupId = 10;
+
+        it('should create a channel', function() {
+            let channelId;
+
+            return client.getVoiceChannelInvite(groupId).then((invite) => {
+                channelId = invite.channel.id;
+                return client.getChannel(channelId);
+            }).then((channel) => {
+                assert.equal(channelId, channel.id);
+            });
+        });
+
+        it('should return a url in the invite', function() {
+            return client.getVoiceChannelInvite(groupId).then((invite) => {
+                assert.isNotNull(invite.url);
+            });
+        });
+    });
+});
